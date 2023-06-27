1086ee1d8796e2da5c11a2ad4137bad8ac12b1f6
James Davison <96993180+jndaviso@users.noreply.github.com>
2023-06-27 15:39:42 -0400

    Update Conditional VAR Forecast - Quarterly GDP Components.R

--- code/Conditional VAR Forecast - Quarterly GDP Components.R
+++ code/Conditional VAR Forecast - Quarterly GDP Components.R
@@ -49,7 +49,7 @@ gdp.quart <- ts_cansim("v62305752", start = date.start)
 target.d <- ts_cansim("v39079", start = date.start)    # daily target rate 
 u <- ts_cansim("v2062815", start = date.start)
 
-fredr_set_key('7b15ffc9ff456a8d5e3e579d2b04a9f8')
+fredr_set_key('')
 wti <- ts_fred('MCOILWTICO', start = date.start) # WTI oil price
 
 # Get Monthly US GDP
